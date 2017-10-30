3721d4d65128db8446f4609fb4c2935efb9197f7
Pavan Kumar <kumarpavan1309@gmail.com>
2017-10-30 15:47:51 +0530

    minor changes

--- app/services/input_file.rb
+++ app/services/input_file.rb
@@ -13,23 +13,17 @@ class InputFile
   end
 
   def createFile
-    somefile = File.open("#{self.name}.#{self.extension}","w")
+    somefile = File.open("temporary/#{self.name}.#{self.extension}","w")
     case self.extension
     when "js"
-      somefile.puts "var fs = require('fs');"
-      somefile.puts "var access = fs.createWriteStream('#{self.name}.txt');"
-      somefile.puts "access.truncate;"
-      somefile.puts "process.stdout.write = process.stderr.write = access.write.bind(access);"
-      somefile.puts "process.on('uncaughtException', function(err) {
-      console.error((err && err.stack) ? err.stack : err);
-      });"
+      somefile.puts "var fs = require('fs'); var access = fs.createWriteStream('temporary/#{self.name}.txt'); access.truncate; process.stdout.write = process.stderr.write = access.write.bind(access); process.on('uncaughtException', function(err) { console.error((err && err.stack) ? err.stack : err); });"
       somefile.puts self.content
     when "py"
-    somefile.puts "import sys"
-    somefile.puts "sys.stdout = sys.stderr = open(\"#{self.name}.txt\",\"w\")"
-    somefile.puts self.content
+      somefile.puts "import sys"
+      somefile.puts "sys.stdout = sys.stderr = open(\"temporary/#{self.name}.txt\",\"w\")"
+      somefile.puts self.content
     else
-    somefile.puts "fname = File.open(\"#{self.name}.txt\",\"w\")"
+    somefile.puts "fname = File.open(\"temporary/#{self.name}.txt\",\"w\")"
     somefile.puts "$stderr = fname"
     somefile.puts "$stdout = fname"
     somefile.puts self.content
@@ -41,19 +35,39 @@ class InputFile
   def execute
     case self.extension
     when "js"
-      cmd = "node #{self.name}.js"
+      cmd = "node temporary/#{self.name}.js"
       `#{cmd}`
     when "py"
-    cmd = "python3 #{self.name}.py"
-    `#{cmd}`
+      cmd = "python3 temporary/#{self.name}.py"
+      `#{cmd}`
     else
-    cmd = "ruby #{self.name}.rb"
+    cmd = "ruby temporary/#{self.name}.rb"
     `#{cmd}`
     end
   end
 
   def respond
-    val = File.read("#{self.name}.txt")
-    return val
+      val = File.read("temporary/#{self.name}.txt")
+      case self.extension
+        when "py"
+          if val.split("line ").size > 1
+            leftpar = val.split("line ")[0]
+            ex = val.split("line ")[1].split("")[0].to_i - 2
+            val = val.split("line ")[1].split("")
+            val.shift
+            rightpar = val.unshift(ex).join("")
+            val = leftpar + "line " + rightpar
+          end
+        else
+          if val.split("rb:").size > 1
+            leftpar = val.split("rb:")[0]
+            ex = val.split("rb:")[1].split("")[0].to_i - 3
+            val = val.split("rb:")[1].split("")
+            val.shift
+            rightpar = val.unshift(ex).join("")
+            val = leftpar + "rb:" + rightpar
+          end
+      end
+      return val
   end
 end
--- python.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.stdout = sys.stderr = open("python.txt","w")
-print ("hello")
